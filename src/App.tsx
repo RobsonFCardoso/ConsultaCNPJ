0c45369f43ccfcc66181b56a277ffd799f501f9d
Robson_Cardoso <osodracoo@gmail.com>
2026-09-14 11:22:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AndroidEmulator } from './components/AndroidEmulator';
 
 export default function App() {
   return (
-    <div className="w-full min-h-screen bg-slate-950 text-slate-100 flex flex-col font-sans">
+    <div className="w-full min-h-screen bg-slate-900 text-slate-100 flex flex-col font-sans">
       <main className="flex-1 w-full h-full flex flex-col">
         <AndroidEmulator />
       </main>
